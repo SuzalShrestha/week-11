8269f687578f2fd6105d9af07d0eabcafe6afcef
suzalshrestha <sujal.scxi121@amv.edu.np>
2023-06-05 20:45:45 +0545

    completed improve-queries

--- Improve-queries/sql-practice/improve-sql-queries.sql
+++ Improve-queries/sql-practice/improve-sql-queries.sql
@@ -22,6 +22,11 @@
 -- Query:
 
     -- Your code here
+    EXPLAIN QUERY PLAN
+    SELECT * FROM cat_toys
+    JOIN cats ON cat_toys.cat_id = cats.id
+    JOIN toys ON cat_toys.toy_id = toys.id
+    WHERE toys.id = 5;
 
 -- Paste your results below (as a comment):
 -- QUERY PLAN
@@ -69,18 +74,26 @@
 -- Create index:
 
     -- Your code here
-    CREATE INDEX idx_cat_toys_toy_id ON cat_toys (toy_id);
+    CREATE INDEX idx_cat_toys_toy_id_cat_id ON cat_toys (toy_id, cat_id);
 
 -- Analyze Query:
     -- Your code here
+    EXPLAIN QUERY PLAN
+    SELECT * FROM cat_toys
+    JOIN cats ON cat_toys.cat_id = cats.id
+    JOIN toys ON cat_toys.toy_id = toys.id
+    WHERE toys.id = 5;
 
 -- Paste your results below (as a comment):
-
+-- QUERY PLAN
+-- |--SEARCH toys USING INTEGER PRIMARY KEY (rowid=?)
+-- |--SEARCH cat_toys USING COVERING INDEX idx_cat_toys_toy_id_cat_id (toy_id=?)
+-- `--SEARCH cats USING INTEGER PRIMARY KEY (rowid=?)
 
 -- Analyze Results:
 
     -- Is the new index being applied in this query?
-
+    -- Yes, the new index is being applied in this query.
 
 
 
@@ -90,19 +103,34 @@
 -- Query (to be used in the sqlite CLI):
 
     -- Your code here
+    .timer on
+    SELECT * FROM cat_toys
+    JOIN cats ON cat_toys.cat_id = cats.id
+    JOIN toys ON cat_toys.toy_id = toys.id
+    WHERE toys.id = 5;
+
 
 -- Paste your results below (as a comment):
+
+-- 10008|31|5|31|Rodger|Lavender|Oregon Rex|5|Shiny Mouse|Blue|7
+-- 10051|77|5|77|Jamal|Orange|Sam Sawet|5|Shiny Mouse|Blue|7
+-- 4509|4002|5|4002|Rachele|Maroon|Foldex Cat|5|Shiny Mouse|Blue|7
+-- Run Time: real 0.001 user 0.000591 sys 0.000000
 
 
 -- Analyze Results:
     -- Are you still getting the correct query results?
-
+    -- Yes, I am still getting the correct query results.
 
     -- Did the execution time improve (decrease)?
-
+    -- Before
+    --Run Time: real 0.003 user 0.000000 sys 0.003137
+    -- After
+    -- Run Time: real 0.001 user 0.000591 sys 0.000000
+    -- Yes, the execution time improved (decreased).
 
     -- Do you see any other opportunities for making this query more efficient?
-
+    -- I do not see any other opportunities for making this query more efficient.
 
 ---------------------------------
 -- Notes From Further Exploration
