8d19741bc6421c74104074e039d524402e51cf21
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-06-14 20:07:02 +0545

    completed modify-tables-short-prac

---
+++ modify-table-short-prac/server/db/migrations/20230614141821-rename-games-numPlayers-and-remove-estPlayTime.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.renameColumn('games', 'numPlayers', 'maxPlayers');
+    await queryInterface.removeColumn('games', 'estPlayTime');
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.renameColumn('games', 'maxPlayers', 'numPlayers');
+    await queryInterface.addColumn('games', 'estPlayTime', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    });
+  }
+};
